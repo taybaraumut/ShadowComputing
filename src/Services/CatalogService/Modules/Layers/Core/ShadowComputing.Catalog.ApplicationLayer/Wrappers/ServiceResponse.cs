cc92c9ea81e426e47ba5105046fd118f52f20da6
Umut Taybara <umut.taybara.info@gmail.com>
2025-03-11 18:09:59 +0300

    added result wrapper

---
+++ src/Services/CatalogService/Modules/Layers/Core/ShadowComputing.Catalog.ApplicationLayer/Wrappers/ServiceResponse.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ShadowComputing.Catalog.ApplicationLayer.Wrappers
+{
+    public class ServiceResponse<T>
+    {
+        public ServiceResponse(T message)
+        {
+            Message = message;
+        }
+
+        public T Message { get; set; }
+    }
+}
